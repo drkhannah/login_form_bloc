98db640f622ddd882125432afb29e981d2072c59
Derek Hannah <drkhannah1@gmail.com>
2018-08-03 13:36:47 -0400

    using broadcast StreamControllers to not get listening error on hot reload

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -4,8 +4,8 @@ import 'validators.dart';
 // you could alternatively use: class Bloc extends Validators {}
 class Bloc extends Object with Validators {
   // Private StreamControllers
-  final _emailController = StreamController<String>();
-  final _passwordController = StreamController<String>();
+  final _emailController = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();
 
   // Add Data to Streams
   Function(String) get changeEmail => _emailController.sink.add;
